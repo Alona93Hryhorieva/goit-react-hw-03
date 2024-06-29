8cd2b3fc108872de25b4377c2d8ab6af399a500d
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-06-30 00:24:30 +0300

    add form and box

--- src/Componens/Contact/Contact.jsx
+++ src/Componens/Contact/Contact.jsx
@@ -1,21 +1,23 @@
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
-// import css from "./Contact.module.css";
+import css from "./Contact.module.css";
 
 export default function Contact({
   contactFrend: { id, name, number },
   onDelete,
 }) {
   return (
-    <div>
-      <p>
-        <FaPhoneAlt /> {name}
-      </p>
-      <p>
-        <FaUser />
-        {number}
-      </p>
+    <>
+      <div>
+        <p className={css.text}>
+          <FaPhoneAlt /> {name}
+        </p>
+        <p className={css.text}>
+          <FaUser />
+          {number}
+        </p>
+      </div>
       <button onClick={() => onDelete(id)}>Delete</button>
-    </div>
+    </>
   );
 }
--- src/Componens/ContactList/ContactList.jsx
+++ src/Componens/ContactList/ContactList.jsx
@@ -1,15 +1,16 @@
 import Contact from "../Contact/Contact";
+import css from "../ContactList/ContactList.module.css";
 
 export default function ContactList({ frends, onDelete }) {
   // console.log(frends);
+
   return (
-    <ul>
+    <ul className={css.list}>
       {frends.map((frend) => (
-        <li key={frend.id}>
-          <Contact contactFrend={frend} />
+        <li key={frend.id} className={css.item}>
+          <Contact contactFrend={frend} onDelete={onDelete} />
         </li>
       ))}
     </ul>
   );
 }
-// console.log(frend.id);
